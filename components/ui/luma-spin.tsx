d5be3ea8f74db169391294874a6ba600a7c2f50e
James Lawson <james@3kpro.services>
2025-12-12 18:25:31 -0600

    fix(ui): revert LumaSpin colors to grayscale per spec

--- components/ui/luma-spin.tsx
+++ components/ui/luma-spin.tsx
@@ -5,9 +5,9 @@ import React from "react";
 export const LumaSpin = () => {
   return (
     <div className="relative w-[65px] aspect-square">
-      <span className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-coral-500 dark:shadow-cyan-400" />
+      <span className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" />
       <span 
-        className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-coral-500 dark:shadow-cyan-400" 
+        className="absolute rounded-[50px] animate-loaderAnim shadow-[inset_0_0_0_3px] shadow-gray-800 dark:shadow-gray-100" 
         style={{ animationDelay: '-1.25s' }}
       />
     </div>
